78571d647e85b24252c76c28deed620bbe1b7cde
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-06-20 17:45:46 -0400

    The Ray website content is here.

---
+++ generate-web-site.sh
@@ -0,0 +1,7 @@
+for page in $(ls page-*)
+do
+	key=$(echo $page|sed 's/page\-//g'|sed 's/.txt//g')
+	htmlFile=$key".html"
+	cp scaffold.txt $htmlFile
+	./generatePage.py $page $htmlFile
+done
---
+++ source.js
@@ -0,0 +1,9 @@
+
+
+function setMouseOver(object){
+	object.className='menuItem_onmouseover';
+}
+
+function setMouseOut(object){
+	object.className='menuItem_onmouseout';
+}
