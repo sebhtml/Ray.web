384769b003299cb0a78b8f24daf48d91e86c5a7c
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2013-10-31 16:00:18 -0400

    add new footer
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- push.sh
+++ push.sh
@@ -3,3 +3,10 @@
 ./generate-web-site.sh
 
 rsync -avz * sebhtml,denovoassembler@web.sourceforge.net:htdocs
+
+git push origin master
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+git checkout master
+
